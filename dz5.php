e7abc68beb3387ee24c7f91d325bfa120df01f93
Klimov <klimov.rv@ya.ru>
2021-08-22 17:41:58 +0300

    test ssh key after change pass

--- dz5.php
+++ dz5.php
@@ -3,10 +3,5 @@ $bmw = ["model" => "X5", "speed" => 120, "doors" => 5, "year" => "2015"];
 $toyota = ["model" => "Camry", "speed" => 150, "doors" => 1, "year" => "2000"];
 $opel = ["model" => "Astra", "speed" => 130, "doors" => 4, "year" => "2005"];
 
-// $key = ["model", "speed", "doors", "year"];
-// $values = ["X5", 120, 5, "2015"];
-// foreach ($bmw as $key => $value) {
-// };
-
 $cars = ["bmw" => $bmw, "toyota" => $toyota, "opel" => $opel];
 echo "<pre>".print_r($cars, 1)."</pre>";
