4d0271bfb5e5d2be69fdc847831d59e45f275750
Klimov Roman <klimov.rv@ya.ru>
2021-08-22 17:54:25 +0300

    test ssh key after change email

--- dz5.php
+++ dz5.php
@@ -4,9 +4,4 @@ $toyota = ["model" => "Camry", "speed" => 150, "doors" => 1, "year" => "2000"];
 $opel = ["model" => "Astra", "speed" => 130, "doors" => 4, "year" => "2005"];
 
 $cars = ["bmw" => $bmw, "toyota" => $toyota, "opel" => $opel];
-echo "<pre>".print_r($cars, 1)."</pre>";
-
-git config --global user.name "Klimov Roman"
-
-
-qgit config --global user.email klimov.rv@ya.ru
+// echo "<pre>".print_r($cars, 1)."</pre>";
