de167bcc260291c9fe81749fe34d529900c4b5ad
Klimov Roman <klimov.rv@ya.ru>
2021-08-22 17:46:35 +0300

    test ssh key after change config

--- dz5.php
+++ dz5.php
@@ -5,3 +5,8 @@ $opel = ["model" => "Astra", "speed" => 130, "doors" => 4, "year" => "2005"];
 
 $cars = ["bmw" => $bmw, "toyota" => $toyota, "opel" => $opel];
 echo "<pre>".print_r($cars, 1)."</pre>";
+
+git config --global user.name "Klimov Roman"
+
+
+qgit config --global user.email klimov.rv@ya.ru
